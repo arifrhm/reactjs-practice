767b7d4b4305f8becb64f719fa7625a5ec068d38
sukmagp <sukmagiri11@gmail.com>
2022-06-04 13:13:41 +0700

    update

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <br>
-          Update Deploy Heroku
-          </br>
         </p>
         <a
           className="App-link"
