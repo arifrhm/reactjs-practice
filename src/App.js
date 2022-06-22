efa245acf1a8ed814bc2939143ec538dc6b63685
sukmagp <sukmagiri11@gmail.com>
2022-06-22 14:13:58 +0700

    Tugas Komponen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import FirstComponent from "./Materi/FirstKomponen";
+import PracticeComponent from "./Tugas/PracticeKomponen";
 
 function App() {
   return (
     <div className="App">
       <FirstComponent/>
+      <PracticeComponent/>
     </div>
   );
 }
---
+++ src/Tugas/PracticeKomponen/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ClassComponent from "./TugasKomponen/ClassComponent";
+import FunctionalComponent from "./TugasKomponen/FunctionalComponent";
+
+export default class PracticeComponent extends React.Component{
+    render() {
+        return(
+            <div>
+                <ClassComponent name= "Sukma Giri Pratama"/>
+                <FunctionalComponent/>
+            </div>
+        )
+    }
+}
